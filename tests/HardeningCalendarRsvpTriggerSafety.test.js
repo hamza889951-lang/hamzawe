fa611b1fc9fa2f051102fcc6d37ff0a20e0c55f2
hamza889951-lang <hamza889951@gmail.com>
2026-09-09 22:16:25 +0300

    test: touch branch for governance rerun

--- tests/HardeningCalendarRsvpTriggerSafety.test.js
+++ tests/HardeningCalendarRsvpTriggerSafety.test.js
@@ -7,6 +7,7 @@ const vm = require('vm');
 
 const ROOT = path.resolve(__dirname, '..');
 
+// Temporary non-functional branch touch for one-time governance execution.
 function load(sourceSandbox) {
   const source = fs.readFileSync(path.join(ROOT, 'Infrastructure/CalendarRsvpTrigger.js'), 'utf8');
   vm.runInContext(source + '\nthis.CalendarRsvpTrigger = CalendarRsvpTrigger;', sourceSandbox, { filename: 'Infrastructure/CalendarRsvpTrigger.js' });
